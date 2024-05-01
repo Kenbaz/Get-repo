d1cb9566116e4777fb1520e10fa8081adec193b8
Kenbaz <kbassey016@gmail.com>
2024-05-02 00:08:40 +0100

    First commit

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,9 @@
+import image from "../assets/images/avatar.JPG"
+
+function Avatar() {
+    return (
+        <img src={image} className="avatar w-12 rounded-full" alt="avatar"/>
+    )
+}
+
+export default Avatar;
