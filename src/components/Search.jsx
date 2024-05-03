dea56e1ea2b433823087fd431a095a710cc92b16
Kenbaz <kbassey016@gmail.com>
2024-05-03 22:15:11 +0100

    height changed on big screen

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -18,7 +18,7 @@ function SearchBar({ onSearch, currentPage, setCurrentPage }) {
   return (
     <div className="w-3/4 rounded-md border border-secondary hover:border-cyan-700 lg:w-3/6 lg:h-11 lg:ms-96">
       <input
-        className="w-full h-full rounded-md placeholder-gray-600 relative pl-2 text-sm font-medium focus:outline-none md:h-14 md:text-2xl md:pl-4 lg:text-base lg:h-10"
+        className="w-full h-full rounded-md placeholder-gray-600 relative pl-2 text-sm font-medium focus:outline-none md:h-14 md:text-2xl md:pl-4 lg:text-base lg:h-full"
         type="text"
         placeholder="Search repository..."
         value={searchQuery}
