b03f6c23617966ca5102e3d27061b656744bd267
Kenbaz <kbassey016@gmail.com>
2024-05-03 22:24:47 +0100

    another adjustment

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -16,9 +16,9 @@ function SearchBar({ onSearch, currentPage, setCurrentPage }) {
   }
 
   return (
-    <div className="w-3/4 rounded-md border border-secondary hover:border-cyan-700 lg:w-3/6 lg:h-11 lg:ms-96">
+    <div className="w-3/4 h-8 rounded-md border border-secondary hover:border-cyan-700 md:h-14 lg:w-3/6 lg:h-11 lg:ms-96">
       <input
-        className="w-full h-full rounded-md placeholder-gray-600 relative pl-2 text-sm font-medium focus:outline-none md:h-14 md:text-2xl md:pl-4 lg:text-base lg:h-full"
+        className="w-full h-full rounded-md placeholder-gray-600 relative pl-2 text-sm font-medium focus:outline-none md:h-full md:text-2xl md:pl-4 lg:text-base lg:h-full"
         type="text"
         placeholder="Search repository..."
         value={searchQuery}
